42c910092aa68fd5df2bbe20fe24da2bebb278b9
Tomas Lingotti <tomaslingotti@hotmail.com>
2019-08-10 00:54:05 -0300

    Writting some comments

--- repository/repository.go
+++ repository/repository.go
@@ -13,7 +13,14 @@ type PostsRepo interface {
 	*/
 	Save(post *model.Post, c elasticsearch.Client) (string, error)
 
+	/**
+	In this example, I use the client and return the response from the elastic-go lib
+	 */
 	FindById(id string, c elasticsearch.Client) (*esapi.Response, error)
 
+	/**
+	Here I use the web client from Go, the query DSL coming from the client - perhaps is no the better idea
+	but fits OK for an example, cURLs for free.
+	 */
 	FindByCriteria(criteria io.Reader) (string, error)
 }
